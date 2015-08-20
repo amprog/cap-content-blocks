d17e074bd490182df214abe6812934797a6802b7
Seth Rubenstein <smrubenstein@gmail.com>
2015-08-20 12:23:16 -0400

    Establish plugin file

---
+++ cap-content-blocks.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: CAP Content Blocks (Shortcodes)
+Plugin URI: http://americanprogress.org
+Description: CAP specific shortcodes for use in long form content.
+Version: 0.1
+Author: Seth Rubenstein for Center for American Progress
+Author URI: http://sethrubenstein.info
+*/
